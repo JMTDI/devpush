c63cacff1a3ffddf95b42e6420d6f32ef2919df1
Joel <carsforall1@gmail.com>
2026-06-21 00:01:37 +0000

    update entrypoint.app.sh to include a check for the existence of the .env file before sourcing it. This prevents errors when the file is missing and allows the script to run without issues.

--- docker/entrypoint.app.sh
+++ docker/entrypoint.app.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-exec uv run uvicorn main:app --host 0.0.0.0 --port 8000 --loop uvloop --workers 4 --proxy-headers --forwarded-allow-ips='*'
+exec uv run uvicorn main:app --host 0.0.0.0 --port 8000 --loop uvloop --workers 1 --proxy-headers --forwarded-allow-ips='*'
